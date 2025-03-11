0c8d124c8b22d1b039e8b5ca4e057cde9fada7b7
ari <ariludkovski@gmail.com>
2025-03-10 22:23:39 -0700

    now only shows matches that are coming soon, and removes old matches.

--- public/script.js
+++ public/script.js
@@ -3,6 +3,50 @@ let teamNumberInput = document.getElementById("teamNumberInput");
 let matchNumberInput = document.getElementById("matchNumberInput");
 let teamColorInput = document.getElementById("teamColorInput");
 
+const eventKey = "demo7228";
+const apiUrl = `https://frc.nexus/api/v1/event/${eventKey}`;
+
+fetch(apiUrl, {
+  method: "GET", // GET request to fetch data
+  headers: {
+    "Nexus-Api-Key": `GEd51Lz5hYot-fIhZM7P84-KPMM`, // Include the Bearer token if required
+    "Content-Type": "application/json", // Typically used to specify data type
+  },
+})
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    return response.json(); // Parse the response as JSON
+  })
+  .then((data) => {
+    const matchSelector = document.getElementById("matchNumberInput");
+    for (let i = 0; i < data.matches.length; i++) {
+      try {
+        if (
+          data.matches[i + 3].status != "On field" &&
+          data.matches[i].status != "Queuing soon"
+        ) {
+          let match = document.createElement("option");
+          match.value = i + 1;
+          match.innerHTML = data.matches[i].label;
+          matchSelector.appendChild(match);
+        }
+      } catch (error) {
+        if (data.matches[i].status != "Queuing soon") {
+          let match = document.createElement("option");
+          match.value = i + 1;
+          match.innerHTML = data.matches[i].label;
+          matchSelector.appendChild(match);
+        }
+      }
+    }
+  })
+  .catch((error) => {
+    console.error("There was a problem with the fetch operation:", error); // Handle any errors
+  });
+
 function loadStoredData() {
   let data = localStorage.getItem("01metaData");
   if (data != null) {
