6b4610ba9a17db5e52b55f20cc27d874af030ca6
ari <ariludkovski@gmail.com>
2025-03-11 21:27:54 -0700

    added fallback in case of missing API data, and manual input button

--- public/script.js
+++ public/script.js
@@ -1,60 +1,82 @@
-let scoutNameInput = document.getElementById("scoutNameInput");
-let teamNumberInput = document.getElementById("teamNumberInput");
-let matchNumberInput = document.getElementById("matchNumberInput");
-let teamColorInput = document.getElementById("teamColorInput");
-
 const eventKey = "demo7228";
 const apiUrl = `https://frc.nexus/api/v1/event/${eventKey}`;
 
-fetch(apiUrl, {
-  method: "GET", // GET request to fetch data
-  headers: {
-    "Nexus-Api-Key": `GEd51Lz5hYot-fIhZM7P84-KPMM`, // Include the Bearer token if required
-    "Content-Type": "application/json", // Typically used to specify data type
-  },
-})
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-
-    return response.json(); // Parse the response as JSON
+const teamInputs = ["Blue 1"];
+function getNexusMatches() {
+  //get nexus api
+  fetch(apiUrl, {
+    method: "GET", // GET request to fetch data
+    headers: {
+      "Nexus-Api-Key": `GEd51Lz5hYot-fIhZM7P84-KPMM`, // Include the Bearer token if required
+      "Content-Type": "application/json", // Typically used to specify data type
+    },
   })
-  .then((data) => {
-    const matchSelector = document.getElementById("matchNumberInput");
-    for (let i = 0; i < data.matches.length; i++) {
-      try {
-        if (
-          data.matches[i + 3].status != "On field" &&
-          data.matches[i].status != "Queuing soon"
-        ) {
-          let match = document.createElement("option");
-          match.value = i + 1;
-          match.innerHTML = data.matches[i].label;
-          matchSelector.appendChild(match);
-        }
-      } catch (error) {
-        if (data.matches[i].status != "Queuing soon") {
-          let match = document.createElement("option");
-          match.value = i + 1;
-          match.innerHTML = data.matches[i].label;
-          matchSelector.appendChild(match);
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      return response.json(); // Parse the response as JSON
+    })
+    .then((data) => {
+      //create dropdown menu for match number
+      const matchSelector = document.createElement("select");
+      const container = document.getElementById("matchInputContainer");
+      matchSelector.name = "matchNumber";
+      matchSelector.id = "matchNumberInput";
+      container.appendChild(matchSelector);
+      for (let i = 0; i < data.matches.length; i++) {
+        try {
+          if (
+            data.matches[i + 2].status != "On field" &&
+            data.matches[i - 2].status != "Queuing soon"
+          ) {
+            let match = document.createElement("option");
+            match.value = i + 1;
+            match.innerHTML = data.matches[i].label;
+            matchSelector.appendChild(match);
+          }
+        } catch (error) {
+          if (data.matches[i].status != "Queuing soon" && i >= 2) {
+            let match = document.createElement("option");
+            match.value = i + 1;
+            match.innerHTML = data.matches[i].label;
+            matchSelector.appendChild(match);
+          } else if (data.matches[i + 2].status != "On field") {
+            let match = document.createElement("option");
+            match.value = i + 1;
+            match.innerHTML = data.matches[i].label;
+            matchSelector.appendChild(match);
+          }
         }
       }
-    }
-  })
-  .catch((error) => {
-    console.error("There was a problem with the fetch operation:", error); // Handle any errors
-  });
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error); // Handle any errors
+      //create typable input for team
+      let oldteamSelector = document.getElementById("teamNumberInput");
+      oldteamSelector.remove();
+      const teamSelector = document.createElement("input");
+      const teamcontainer = document.getElementById("teamInputContainer");
+      teamSelector.placeholder = "Team Number";
+      teamSelector.id = "teamNumberInput";
+      teamcontainer.appendChild(teamSelector);
 
+      //create typable input for match
+      const matchSelector = document.createElement("input");
+      const container = document.getElementById("matchInputContainer");
+      matchSelector.placeholder = "Match Number";
+      matchSelector.id = "matchNumberInput";
+      container.appendChild(matchSelector);
+    });
+  let matchNumberInput = document.getElementById("matchNumberInput");
+}
 function loadStoredData() {
   let data = localStorage.getItem("01metaData");
   if (data != null) {
     let metaData = JSON.parse(data);
     scoutNameInput.value = metaData.scoutName;
     teamColorInput.value = metaData.teamColor;
-    matchNumberInput.value = metaData.matchNumber;
-    teamNumberInput.value = metaData.teamNumber;
   }
 }
 
@@ -66,6 +88,29 @@ function saveData() {
   metaData.teamColor = teamColorInput.value;
   localStorage.setItem("01metaData", JSON.stringify(metaData));
 }
+function setManual() {
+  //create typable input for team
+  let oldteamSelector = document.getElementById("teamNumberInput");
+  oldteamSelector.remove();
+  const teamSelector = document.createElement("input");
+  const teamcontainer = document.getElementById("teamInputContainer");
+  teamSelector.placeholder = "Team Number";
+  teamSelector.id = "teamNumberInput";
+  teamcontainer.appendChild(teamSelector);
 
+  //create typable input for match
+  let oldmatchSelectior = document.getElementById("matchNumberInput");
+  oldmatchSelectior.remove();
+  const matchSelector = document.createElement("input");
+  const container = document.getElementById("matchInputContainer");
+  matchSelector.placeholder = "Match Number";
+  matchSelector.id = "matchNumberInput";
+  container.appendChild(matchSelector);
+}
+getNexusMatches();
+let scoutNameInput = document.getElementById("scoutNameInput");
+let teamNumberInput = document.getElementById("teamNumberInput");
+
+let teamColorInput = document.getElementById("teamColorInput");
 loadStoredData();
 localStorage.clear();
