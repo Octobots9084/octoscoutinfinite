17f2a9664e5afe41f400ce584e6477b935d56ef3
ari <ariludkovski@gmail.com>
2025-03-31 21:05:42 -0700

    output now shows errors

--- public/outputView/script.js
+++ public/outputView/script.js
@@ -19,6 +19,7 @@ async function createDataBlocks() {
   await updateJSON();
   let container = document.getElementById("dataContainer");
   container.innerHTML = "";
+  let errors = 0;
   for (let i = 0; i < parsedJSONOutput.length; i++) {
     if (!parsedJSONOutput[i].deleted) {
       //create container
@@ -38,149 +39,161 @@ async function createDataBlocks() {
         createDataBlocks();
       };
       wrapper.appendChild(clickableDeleteImage);
+      try {
+        //show important data
+        let metaData = parsedJSONOutput[i]["01metaData"];
+        let metaDataDisplay = document.createElement("div");
+        metaDataDisplay.classList.add("dataHolder");
+        wrapper.appendChild(metaDataDisplay);
+        metaDataDisplay.innerHTML =
+          "Scout Name: " +
+          metaData.scoutName +
+          " | Team Number: " +
+          metaData.teamNumber +
+          " | Match: " +
+          metaData.matchNumber;
 
-      //show important data
-      let metaData = parsedJSONOutput[i]["01metaData"];
-      let metaDataDisplay = document.createElement("div");
-      metaDataDisplay.classList.add("dataHolder");
-      wrapper.appendChild(metaDataDisplay);
-      metaDataDisplay.innerHTML =
-        "Scout Name: " +
-        metaData.scoutName +
-        " | Team Number: " +
-        metaData.teamNumber +
-        " | Match: " +
-        metaData.matchNumber;
+        //show starting location
+        let startingLocation = parsedJSONOutput[i]["02startingLocation"];
+        let startingLocationDisplay = document.createElement("div");
+        startingLocationDisplay.classList.add("dataHolder");
+        wrapper.appendChild(startingLocationDisplay);
+        startingLocationDisplay.innerHTML =
+          "Starting Location: " + startingLocation.name;
+        //show autonomous results
 
-      //show starting location
-      let startingLocation = parsedJSONOutput[i]["02startingLocation"];
-      let startingLocationDisplay = document.createElement("div");
-      startingLocationDisplay.classList.add("dataHolder");
-      wrapper.appendChild(startingLocationDisplay);
-      startingLocationDisplay.innerHTML =
-        "Starting Location: " + startingLocation.name;
-      //show autonomous results
+        let auto = parsedJSONOutput[i]["03auto"];
+        console.log(auto);
+        let autoDisplay = document.createElement("div");
+        autoDisplay.classList.add("dataHolder", "collapsible");
+        wrapper.appendChild(autoDisplay);
+        autoDisplay.innerHTML = "Click to View Auto Data";
+        autoDisplay.style.paddingBottom = "5px";
 
-      let auto = parsedJSONOutput[i]["03auto"];
-      let autoDisplay = document.createElement("div");
-      autoDisplay.classList.add("dataHolder", "collapsible");
-      wrapper.appendChild(autoDisplay);
-      autoDisplay.innerHTML = "Click to View Auto Data";
-      autoDisplay.style.paddingBottom = "5px";
+        for (let j = 0; j < auto.length; j++) {
+          //creating the cointainer for the piece
 
-      for (let j = 0; j < auto.length; j++) {
-        //creating the cointainer for the piece
+          let piece = auto[j];
+          let pieceContainer = document.createElement("div");
+          pieceContainer.classList.add("gamePiece", "content");
+          //creating the image of the piece
 
-        let piece = auto[j];
-        let pieceContainer = document.createElement("div");
-        pieceContainer.classList.add("gamePiece", "content");
-        //creating the image of the piece
+          let pieceImage = document.createElement("img");
+          pieceImage.classList.add("pieceImage");
+          pieceImage.setAttribute("src", "../images/" + piece.name + ".png");
+          pieceContainer.appendChild(pieceImage);
+          autoDisplay.appendChild(pieceContainer);
 
-        let pieceImage = document.createElement("img");
-        pieceImage.classList.add("pieceImage");
-        pieceImage.setAttribute("src", "../images/" + piece.name + ".png");
-        pieceContainer.appendChild(pieceImage);
-        autoDisplay.appendChild(pieceContainer);
-
-        //showing data about the piece
-        let pieceInfo = document.createElement("div");
-        pieceContainer.appendChild(pieceInfo);
-        pieceContainer.classList.add("pieceInfo");
-        pieceInfo.innerHTML =
-          "Collected: " +
-          piece.collectionLocation.name +
-          " | Result: " +
-          piece.result;
-      }
-
-      if (auto.length == 0) {
-        autoDisplay.innerHTML = "No Auto Data";
-        autoDisplay.classList.remove("collapsible");
-      }
-
-      //show teleoperated results
-      let teleop = parsedJSONOutput[i]["04teleop"];
-      let teleopDisplay = document.createElement("div");
-      teleopDisplay.classList.add("dataHolder", "collapsible");
-      wrapper.appendChild(teleopDisplay);
-      teleopDisplay.innerHTML = "Click to View Teleop Data";
-      teleopDisplay.style.paddingBottom = "5px";
-
-      for (let j = 0; j < teleop.length; j++) {
-        //creating the cointainer for the piece
-        let piece = teleop[j];
-        let pieceContainer = document.createElement("div");
-        pieceContainer.classList.add("gamePiece", "content");
-
-        //creating the image of the piece
-        let pieceImage = document.createElement("img");
-        pieceImage.classList.add("pieceImage");
-        pieceImage.setAttribute("src", "../images/" + piece.name + ".png");
-        pieceContainer.appendChild(pieceImage);
-        teleopDisplay.appendChild(pieceContainer);
-
-        //showing data about the piece
-        let pieceInfo = document.createElement("div");
-        pieceContainer.appendChild(pieceInfo);
-        pieceContainer.classList.add("pieceInfo");
-        pieceInfo.innerHTML =
-          "Collected: " +
-          piece.collectionLocation.name +
-          " | Result: " +
-          piece.result;
-      }
-
-      if (teleop.length == 0) {
-        teleopDisplay.innerHTML = "No Teleop Data";
-        teleopDisplay.classList.remove("collapsible");
-      }
-
-      //show endgame data
-      let endgame = parsedJSONOutput[i]["05endgame"];
-      let endgameDisplay = document.createElement("div");
-      endgameDisplay.classList.add("dataHolder");
-      wrapper.appendChild(endgameDisplay);
-      endgameDisplay.innerHTML =
-        "Deep: " +
-        endgame.Deep +
-        " | Shallow: " +
-        endgame.Shallow +
-        " | Park: " +
-        endgame.Park;
-
-      //show extra data
-      let extra = parsedJSONOutput[i]["06extra"];
-      let extraDisplay = document.createElement("div");
-      extraDisplay.classList.add("dataHolder");
-      wrapper.appendChild(extraDisplay);
-      extraDisplay.innerHTML =
-        "Died: " +
-        extra.Died +
-        " | Defense: " +
-        extra.Defense +
-        " | Driver Quality: " +
-        extra.Driver_Quality;
-
-      //show comments
-
-      let commentDisplay = document.createElement("div");
-      commentDisplay.classList.add("dataHolder");
-      wrapper.appendChild(commentDisplay);
-      commentDisplay.innerHTML = "Comment : " + extra.Comments;
-
-      //find anomalies
-      var anomalies = detectAnomalies();
-      if (anomalies[metaData.matchNumber] != 6) {
-        if (anomalies[metaData.matchNumber] > 6) {
-          wrapper.classList.add("anomalyBig");
-        } else if (anomalies[metaData.matchNumber] < 6) {
-          wrapper.classList.add("anomalySmall");
+          //showing data about the piece
+          let pieceInfo = document.createElement("div");
+          pieceContainer.appendChild(pieceInfo);
+          pieceContainer.classList.add("pieceInfo");
+          pieceInfo.innerHTML =
+            "Collected: " +
+            piece.collectionLocation.name +
+            " | Result: " +
+            piece.result;
         }
-        let anomalyDisplay = document.createElement("div");
-        anomalyDisplay.classList.add("dataHolder");
-        wrapper.appendChild(anomalyDisplay);
-        anomalyDisplay.innerHTML =
-          "Number of results: " + anomalies[metaData.matchNumber];
+
+        if (auto.length == 0) {
+          autoDisplay.innerHTML = "No Auto Data";
+          autoDisplay.classList.remove("collapsible");
+        }
+
+        //show teleoperated results
+        let teleop = parsedJSONOutput[i]["04teleop"];
+        let teleopDisplay = document.createElement("div");
+        teleopDisplay.classList.add("dataHolder", "collapsible");
+        wrapper.appendChild(teleopDisplay);
+        teleopDisplay.innerHTML = "Click to View Teleop Data";
+        teleopDisplay.style.paddingBottom = "5px";
+
+        for (let j = 0; j < teleop.length; j++) {
+          //creating the cointainer for the piece
+          let piece = teleop[j];
+          let pieceContainer = document.createElement("div");
+          pieceContainer.classList.add("gamePiece", "content");
+
+          //creating the image of the piece
+          let pieceImage = document.createElement("img");
+          pieceImage.classList.add("pieceImage");
+          pieceImage.setAttribute("src", "../images/" + piece.name + ".png");
+          pieceContainer.appendChild(pieceImage);
+          teleopDisplay.appendChild(pieceContainer);
+
+          //showing data about the piece
+          let pieceInfo = document.createElement("div");
+          pieceContainer.appendChild(pieceInfo);
+          pieceContainer.classList.add("pieceInfo");
+          pieceInfo.innerHTML =
+            "Collected: " +
+            piece.collectionLocation.name +
+            " | Result: " +
+            piece.result;
+        }
+
+        if (teleop.length == 0) {
+          teleopDisplay.innerHTML = "No Teleop Data";
+          teleopDisplay.classList.remove("collapsible");
+        }
+
+        //show endgame data
+        let endgame = parsedJSONOutput[i]["05endgame"];
+        let endgameDisplay = document.createElement("div");
+        endgameDisplay.classList.add("dataHolder");
+        wrapper.appendChild(endgameDisplay);
+        endgameDisplay.innerHTML =
+          "Deep: " +
+          endgame.Deep +
+          " | Shallow: " +
+          endgame.Shallow +
+          " | Park: " +
+          endgame.Park;
+
+        //show extra data
+        let extra = parsedJSONOutput[i]["06extra"];
+        let extraDisplay = document.createElement("div");
+        extraDisplay.classList.add("dataHolder");
+        wrapper.appendChild(extraDisplay);
+        extraDisplay.innerHTML =
+          "Died: " +
+          extra.Died +
+          " | Defense: " +
+          extra.Defense +
+          " | Driver Quality: " +
+          extra.Driver_Quality;
+
+        //show comments
+
+        let commentDisplay = document.createElement("div");
+        commentDisplay.classList.add("dataHolder");
+        wrapper.appendChild(commentDisplay);
+        commentDisplay.innerHTML = "Comment : " + extra.Comments;
+
+        //find anomalies
+        var anomalies = detectAnomalies();
+        if (anomalies[metaData.matchNumber] != 6) {
+          if (anomalies[metaData.matchNumber] > 6) {
+            wrapper.classList.add("anomalyBig");
+          } else if (anomalies[metaData.matchNumber] < 6) {
+            wrapper.classList.add("anomalySmall");
+          }
+          let anomalyDisplay = document.createElement("div");
+          anomalyDisplay.classList.add("dataHolder");
+          wrapper.appendChild(anomalyDisplay);
+          anomalyDisplay.innerHTML =
+            "Number of results: " + anomalies[metaData.matchNumber];
+        }
+      } catch (e) {
+        let errorDisplay = document.createElement("div");
+        errorDisplay.classList.add("dataHolder");
+        errorDisplay.style.fontSize = "larger";
+        errorDisplay.style.backgroundColor = "red";
+
+        wrapper.appendChild(errorDisplay);
+        wrapper.style.backgroundColor = "red";
+        errorDisplay.innerHTML += "ERROR: " + e;
+        errors++;
       }
     }
   }
@@ -205,7 +218,9 @@ async function createDataBlocks() {
     " | Matches with less scouts: " +
     anomalousSmall +
     " | Matches with too many scouts: " +
-    anomalousBig;
+    anomalousBig +
+    "| Errors: " +
+    errors;
   createCollapsibleElements();
 }
 async function removeData(index) {
@@ -223,7 +238,7 @@ async function removeData(index) {
       alert("Match Removed");
       console.log(parsedJSONOutput[index]);
       //if (Math.random() < 0.5) {
-        var sound = new Audio("../images/pop.mp3");
+      var sound = new Audio("../images/pop.mp3");
       // } else {
       //   var sound = new Audio("popRare.mp3");
       // }
@@ -275,23 +290,23 @@ function createCollapsibleElements() {
 createDataBlocks();
 createCollapsibleElements();
 function playConfetti() {
-  const confettiImage = document.createElement('img');
-  
-  confettiImage.src = '../images/confetti.gif';
+  const confettiImage = document.createElement("img");
+
+  confettiImage.src = "../images/confetti.gif";
   confettiImage.width = 1536;
   confettiImage.height = 1536;
-  
-  confettiImage.style.position = 'fixed';
-  confettiImage.style.top = '50%';
-  confettiImage.style.left = '50%';
-  confettiImage.style.transform = 'translate(-50%, -50%)';
-  confettiImage.style.zIndex = '9999';
-  
+
+  confettiImage.style.position = "fixed";
+  confettiImage.style.top = "50%";
+  confettiImage.style.left = "50%";
+  confettiImage.style.transform = "translate(-50%, -50%)";
+  confettiImage.style.zIndex = "9999";
+
   document.body.appendChild(confettiImage);
-  
+
   setTimeout(() => {
     confettiImage.remove();
-  }, 1500);//1.5 sec
+  }, 1500); //1.5 sec
 }
 let currentCupcakeImage = null;
 
@@ -304,19 +319,19 @@ function playCupcake() {
   let randomX = Math.random() * 100;
   let randomY = Math.random() * 100;
 
-  const cupcakeImage = document.createElement('img');
-  
-  cupcakeImage.src = '../images/cupcake.png';
+  const cupcakeImage = document.createElement("img");
+
+  cupcakeImage.src = "../images/cupcake.png";
   cupcakeImage.width = 1152 / 4;
   cupcakeImage.height = 648 / 4;
-  
-  cupcakeImage.style.position = 'fixed';
-  cupcakeImage.style.top = randomY + '%';
-  cupcakeImage.style.left = randomX + '%';
-  cupcakeImage.style.transform = 'translate(-50%, -50%)';
-  cupcakeImage.style.zIndex = '9999';
-  
+
+  cupcakeImage.style.position = "fixed";
+  cupcakeImage.style.top = randomY + "%";
+  cupcakeImage.style.left = randomX + "%";
+  cupcakeImage.style.transform = "translate(-50%, -50%)";
+  cupcakeImage.style.zIndex = "9999";
+
   document.body.appendChild(cupcakeImage);
-  
+
   currentCupcakeImage = cupcakeImage;
 }
