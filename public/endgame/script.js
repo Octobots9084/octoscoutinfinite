c898be370a2ad49a4ca48136462cc4880d4d4953
ari <ariludkovski@gmail.com>
2025-09-21 21:19:39 -0700

    added clickable images for endgame

--- public/endgame/script.js
+++ public/endgame/script.js
@@ -1,59 +1,52 @@
-import { getJSONConfig } from "/util.js";
+import {
+  getJSONConfig,
+  xPositionMetersToPixelsFromTop,
+  yPositionMetersToPixelsFromLeft,
+  fieldWidth,
+  fieldHeight,
+} from "/util.js";
 
 let JSONConfig = await getJSONConfig();
 document.title = JSONConfig.pageTitle;
-let endgameOptions = JSONConfig.endgameOptions;
-let endgameSelects = [];
+let fieldContainer = document.getElementById("fieldContainer");
+let fieldImage = document.getElementById("fieldImage");
+let isBlue = JSON.parse(localStorage.getItem("01metaData")).teamColor == "Blue";
+generateEndgameButtons();
+document.getElementById("teamNum").innerHTML =
+  "Team #: " + JSON.parse(localStorage.getItem("01metaData")).teamNumber;
 
-// Get the form container instead of appending to body
-const formContainer = document.getElementById("endgame-form");
+// Dynamically generating buttons to select starting location
+function generateEndgameButtons() {
+  let endgames = JSONConfig.endgameOptions;
 
-generateSelects();
-loadStoredData();
-
-function loadStoredData() {
-  let data = localStorage.getItem("05endgame");
-  if (data != null) {
-    let endgameData = JSON.parse(data);
-    for (let i = 0; i < endgameOptions.length; i++) {
-      endgameSelects[i].value = endgameData[endgameOptions[i].name];
-    }
+  for (let i = 0; i < endgames.length; i++) {
+    addEndgameButton(endgames[i]);
   }
 }
-
-// Dynamically generating select elements
-function generateSelects() {
-  for (let i = 0; i < endgameOptions.length; i++) {
-    let container = document.createElement("div");
-    container.classList.add("inputContainer");
-
-    endgameSelects.push(document.createElement("select"));
-
-    let label = document.createElement("h3");
-    label.innerHTML = endgameOptions[i].name;
-    label.classList.add("inputLabel");
-
-    let possibleResults = endgameOptions[i].possibleResults;
-    for (var j = 0; j < possibleResults.length; j++) {
-      var option = document.createElement("option");
-      option.value = possibleResults[j].name;
-      option.text = possibleResults[j].name;
-      endgameSelects[endgameSelects.length - 1].appendChild(option);
-    }
-
-    container.appendChild(label);
-    container.appendChild(endgameSelects[endgameSelects.length - 1]);
-
-    // Append to form container instead of body
-    formContainer.appendChild(container);
+// Adding a button to select a starting location
+function addEndgameButton(endgame) {
+  if (isBlue) {
+    endgame.x = fieldHeight - endgame.x;
+    endgame.y = fieldWidth - endgame.y;
   }
+  // Creating the button
+  let button = document.createElement("button");
+  button.onclick = function () {
+    selectEndgame(endgame);
+    window.location.href = "/submit";
+  };
+
+  button.classList.add("positionButton");
+
+  // Calculating position on the field
+  button.style.top =
+    xPositionMetersToPixelsFromTop(fieldImage, endgame.x, 5) + "px";
+  button.style.left =
+    yPositionMetersToPixelsFromLeft(fieldImage, endgame.y, 5) + "px";
+
+  fieldContainer.appendChild(button);
 }
 
-window.saveData = saveData;
-function saveData() {
-  let endgame = {};
-  for (let i = 0; i < endgameOptions.length; i++) {
-    endgame[endgameOptions[i].name] = endgameSelects[i].value;
-  }
-  localStorage.setItem("05endgame", JSON.stringify(endgame));
+function selectEndgame(endgame) {
+  localStorage.setItem("05endgame", JSON.stringify([endgame]));
 }
