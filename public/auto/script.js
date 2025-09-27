d60e1784eec20e1b9a49324f0c34d9e0e4f3a703
ari <ariludkovski@gmail.com>
2025-09-27 10:30:21 -0700

    maybe load better?

--- public/auto/script.js
+++ public/auto/script.js
@@ -15,8 +15,13 @@ let gamePieceViewer = document.getElementById("gamePieceViewer");
 let gamePieces = [];
 document.getElementById("teamNum").innerHTML =
   "Team #: " + JSON.parse(localStorage.getItem("01metaData")).teamNumber;
-
-generateCollectionButtons();
+if (fieldImage.complete) {
+  generateCollectionButtons();
+} else {
+  fieldImage.onload = () => {
+    generateCollectionButtons();
+  };
+}
 loadStoredData();
 class GamePiece {
   constructor(collectionLocation, name, color) {
